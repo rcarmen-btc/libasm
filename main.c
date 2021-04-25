2a61747908b6c7c6b98ca28786278b1f686cd63e
rcarmen <rcarmen@student.21-school.ru>
2021-04-25 21:28:45 +0300

    Done

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ ssize_t		ft_write(int fd, const void *buf, size_t count);
 ssize_t		ft_read(int fd, void *buf, size_t count);
 char		*ft_strdup(const char *s);
 
-void	check_ft_strlen()
+int main()
 {
 	char *s[10];
 	char d[10][2500];
@@ -36,46 +36,47 @@ void	check_ft_strlen()
 		printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
 
 
-		printf("========(FT_)STRLEN========\n");		
+		printf("===================(FT_)STRLEN===================\n");		
 		printf("strlen:         %ld\nft_strlen:      %ld\n", strlen(s[i]), ft_strlen(s[i]));
 
 
-		printf("========(FT_)STRCPY========\n");		
+		printf("===================(FT_)STRCPY===================\n");		
 		printf("strcpy:        |%s|\nft_strcpy:     |%s|\n", strcpy(d[i], s[i]), ft_strcpy(ft_d[i], s[i]));	
 
 
-		printf("========(FT_)STRCMP========\n");		
+		printf("===================(FT_)STRCMP===================\n");		
 		printf("strcmp:        %d\nft_strcmp:     %d\n", strcmp(d[i], ft_d[i]), ft_strcmp(d[i], ft_d[i]));
 
 
-		printf("========(FT_)STRDUP========\n");		
+		printf("===================(FT_)STRDUP===================\n");		
 		printf("strdup:        |%s|\nft_strdup:     |%s|\n", strdup(s[i]), ft_strdup(s[i]));
-
-
-		printf("========(FT_)WRITE========\n");		
-		printf("INVALID FD\n");
-		errno = 0;
-		printf("write:\n   streerror - %s\n   errno - %d\n   fd - %zd\n", strerror(errno), errno, write(5, "test\n", 6));
-		errno = 0;
-		printf("ft_write:\n   streerror - %s\n   errno - %d\n   ft - %zd\n", strerror(errno), errno, ft_write(5, "test\n", 6));
-		printf("\n");
-		printf("INVALID STR\n");
-		errno = 0;
-		printf("write:\n   streerror - %s\n   errno - %d\n   fd - %zd\n", strerror(errno), errno, write(1, NULL, 6));
-		errno = 0;
-		printf("ft_write:\n   streerror - %s\n   errno - %d\n   ft - %zd\n", strerror(errno), errno, ft_write(1, NULL, 6));
-		printf("\n");
-		printf("WORKING FD\n\n");
-		write(1, "write:         |", 17);
-		len = write(1, s[i], strlen(s[i]));
-		write(1, "|\n", 2);
-		write(1, "ft_write:      |", 17);
-		ft_len = ft_write(1, s[i], strlen(s[i]));
-		write(1, "|\n", 2);
-		printf("write:         %d\nft_write:      %d\n", len, ft_len);		
 		i++;
 	}
-	printf("========(FT_)READ========\n");		
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+
+	printf("===================(FT_)WRITE===================\n");		
+	printf("INVALID FD\n");
+	errno = 0;
+	printf("write:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, write(5, "test\n", 6));
+	errno = 0;
+	printf("ft_write:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, ft_write(5, "test\n", 6));
+	printf("\n");
+	printf("INVALID STR\n");
+	errno = 0;
+	printf("write:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, write(1, NULL, 6));
+	errno = 0;
+	printf("ft_write:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, ft_write(1, NULL, 6));
+	printf("\n");
+	printf("WORKING FD\n");
+	write(1, "write:         |", 17);
+	len = write(1, s[7], strlen(s[7]));
+	write(1, "|\n", 2);
+	write(1, "ft_write:      |", 17);
+	ft_len = ft_write(1, s[7], strlen(s[7]));
+	write(1, "|\n", 2);
+	printf("write:         %d\nft_write:      %d\n", len, ft_len);		
+
+	printf("===================(FT_)READ===================\n");		
 	char *buf = calloc(340, 1);
 	char *ft_buf = calloc(340, 1);
 	int fd;
@@ -84,24 +85,17 @@ void	check_ft_strlen()
 	ft_fd = open("main.c", O_RDONLY);
 	printf("INVALID FD\n");
 	errno = 0;
-	printf("read:\n   streerror - %s\n   errno - %d\n   fd - %zd\n", strerror(errno), errno, read(5, "test\n", 6));
+	printf("read:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, read(5, "test\n", 6));
 	errno = 0;
-	printf("ft_read:\n   streerror - %s\n   errno - %d\n   ft - %zd\n", strerror(errno), errno, ft_read(5, "test\n", 6));
+	printf("ft_read:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, ft_read(5, "test\n", 6));
 	printf("\n");
 	printf("INVALID BUFF\n");
 	errno = 0;
-	printf("read:\n   streerror - %s\n   errno - %d\n   fd - %zd\n", strerror(errno), errno, read(fd, NULL, 6));
+	printf("read:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, read(fd, NULL, 6));
 	errno = 0;
-	printf("ft_read:\n   streerror - %s\n   errno - %d\n   ft - %zd\n", strerror(errno), errno, ft_read(fd, NULL, 6));
+	printf("ft_read:\n   streerror - %s\n   errno - %d\n   ret - %zd\n", strerror(errno), errno, ft_read(fd, NULL, 6));
 	printf("\n");
 	printf("WORKING FD\n\n");
 	printf("read:\n---------------\n%s\n---------------\nret: %zd\n===============\n", buf, read(fd, buf, 200));
 	printf("ft_read:\n---------------\n%s\n---------------\nret: %zd\n", ft_buf, read(ft_fd, ft_buf, 200));
-	printf("\n\n");
-}
-
-int main()
-{
-	
-	check_ft_strlen();
 }
